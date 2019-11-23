db49e553844dd2a09435fd01ccd87811e24c3e92
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2019-11-23 18:22:59 -0300

    Initial Commit

---
+++ AWSTranscribeBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AWSTranscribeBug
+//
+//  Created by João Souza on 23/11/19.
+//  Copyright © 2019 Example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
